cd6629d3f57a51e99c0d0e451fbdeb933ce65ec5
orgoro <or@d-id.com>
2021-01-19 20:23:33 +0200

    init: add prints

--- services/localstack/init.sh
+++ services/localstack/init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "Init localstack s3"
-
+echo "[!] Init localstack s3://visitors"
 awslocal s3 mb s3://visitors
+
+echo "[!] Init localstack sns://visit"
 awslocal sns create-topic --name visit
+
+echo "[!] Localstack READY for Batman..."
