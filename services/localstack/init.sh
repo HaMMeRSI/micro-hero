67e6c265f8ff4b485de2e2ea00e9e1d9e9071188
orgoro <or@d-id.com>
2021-02-07 10:27:25 +0200

    .gitattributs lf

--- services/localstack/init.sh
+++ services/localstack/init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# This file should be in LF format line separators - even in windows!
 
 echo "[!] Init localstack s3://visitors"
 awslocal s3 mb s3://visitors
@@ -6,4 +7,7 @@ awslocal s3 mb s3://visitors
 echo "[!] Init localstack sns://visit"
 awslocal sns create-topic --name visit
 
-echo "[!] Localstack READY for Batman..."
+echo "|================================================================================================"
+echo "| Localstack is READY for Batman"
+echo "| Make sure all your code runs after this line >>>"
+echo "|================================================================================================"
