c2c79cb63a1a00f7f62c6d1e67714f695d6a9cc4
Ore Poran <oreporan@gmail.com>
2021-09-29 16:21:25 +0300

    use TF with example

--- services/detect/src/opencv/example.js
+++
@@ -1,7 +0,0 @@
-const getDetection = require('./get-detection');
-const loadOpenCv = require('./loader');
-
-loadOpenCv()
-    .then(() => getDetection(`${__dirname}/batman.jpg`))
-    .then(detection => console.log(`detection: ${detection}`))
-    .catch(console.error);
